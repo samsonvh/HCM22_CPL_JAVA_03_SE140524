5b55290094ff2d7ce35227dd1a7a6e46444ff1ed
samsonvh <samson.vh.20@gmail.com>
2022-05-10 09:36:31 +0700

    add Exercise1

---
+++ Exercise1/src/exercise/Exercise1.java
@@ -0,0 +1,12 @@
+package exercise;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1 = 125;
+		int number2 = 2019;
+		System.out.println("Sum: " + (number1 + number2));
+	}
+
+}
